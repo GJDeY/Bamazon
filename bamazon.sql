ae468289734a7ea4ba420ed8dabb17fc36c90db5
GJDeY <gregoryjdeyoung@u.northwestern.edu>
2018-09-06 17:05:48 -0500

    added some mySql code

---
+++ bamazon.sql
@@ -0,0 +1,20 @@
+-- database info copied from mySql workbench
+
+CREATE DATABASE BAMAZON;
+USE BAMAZON;
+
+ -- created products table
+-- id, name
+CREATE TABLE PRODUCTS (
+	ITEM_ID INTEGER(11) auto_increment NOT NULL,
+    PRODUCT_NAME VARCHAR(30),
+   DEPARTMENT_NAME VARCHAR(30),
+   PRICE INTEGER(11),
+   STOCK_QUANTITY INTEGER(11),
+   
+   primary key (ITEM_ID)
+);
+select * from products
+
+--Inserting data into the 'products' table
+INSERT INTO products(product_name, department_name, price);
